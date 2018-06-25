f419b1278d62d2bf6dae8f573aaab98a937f57b5
IsaacShrestha <ishrestha@unomaha.edu>
2018-06-25 18:26:38 -0500

    skeleton - ng talking to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCEXhmMqV0iEnBJ5heCGB0kZc75jhXLBCg',
+    authDomain: 'businesscontacts-f785e.firebaseapp.com',
+    databaseURL: 'https://businesscontacts-f785e.firebaseio.com',
+    projectId: 'businesscontacts-f785e',
+    storageBucket: 'businesscontacts-f785e.appspot.com',
+    messagingSenderId: '936083290742'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebase: {
+    apiKey: 'AIzaSyCEXhmMqV0iEnBJ5heCGB0kZc75jhXLBCg',
+    authDomain: 'businesscontacts-f785e.firebaseapp.com',
+    databaseURL: 'https://businesscontacts-f785e.firebaseio.com',
+    projectId: 'businesscontacts-f785e',
+    storageBucket: 'businesscontacts-f785e.appspot.com',
+    messagingSenderId: '936083290742'
+  }
 };
 
 /*
