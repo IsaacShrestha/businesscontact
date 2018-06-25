a052fad0459c938bdafc8b355b9018cc6918eb3d
IsaacShrestha <ishrestha@unomaha.edu>
2018-06-25 18:38:13 -0500

    few changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyCEXhmMqV0iEnBJ5heCGB0kZc75jhXLBCg',
-    authDomain: 'businesscontacts-f785e.firebaseapp.com',
-    databaseURL: 'https://businesscontacts-f785e.firebaseio.com',
-    projectId: 'businesscontacts-f785e',
-    storageBucket: 'businesscontacts-f785e.appspot.com',
-    messagingSenderId: '936083290742'
-  }
+
 };
